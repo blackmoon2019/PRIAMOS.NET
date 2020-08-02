9064f60f25c31d004a5a0439e12fc8e771f9aeff
blackmoon-home <johnmavroselinos@gmail.com>
2020-08-02 22:00:25 +0300

    mainforms create

---
+++ PRIAMOS .NET/PRIAMOS .NET/classes/CN.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Configuration
+Imports System.Data.SqlClient
+
+Public Class CN
+    Private connStr As String = ConfigurationManager.ConnectionStrings("myConnectionString").ConnectionString
+    Public Function OpenConnection() As Boolean
+        Dim DBConnection As New SqlConnection()
+        Try
+            DBConnection.ConnectionString = connStr
+            DBConnection.Open()
+            If DBConnection.State = ConnectionState.Open Then
+                CNDB = DBConnection
+                Return True
+            Else
+                DBConnection.Close()
+                Return False
+            End If
+        Catch ex As Exception
+            MessageBox.Show(String.Format("Error: {0}", ex.Message))
+            Return False
+        End Try
+    End Function
+
+End Class
---
+++ PRIAMOS .NET/PRIAMOS .NET/forms/frmMain.vb
@@ -0,0 +1,18 @@
+﻿Imports DevExpress.XtraEditors
+Imports DevExpress.XtraTabbedMdi
+Public Class frmMain
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles Me.Load
+    End Sub
+
+    Private Sub MdiManager_PageAdded(sender As Object, e As MdiTabPageEventArgs)
+        Dim page As XtraMdiTabPage = e.Page
+        page.Tooltip = "Tooltip for the page " + page.Text
+    End Sub
+    Private Sub bbUsers_ItemClick(sender As Object, e As DevExpress.XtraBars.ItemClickEventArgs) Handles bbUsers.ItemClick
+        Dim f As frmLogin = New frmLogin()
+
+        f.Text = "Child Form "
+        f.MdiParent = Me
+        f.Show()
+    End Sub
+End Class
